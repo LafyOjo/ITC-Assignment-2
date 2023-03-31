51b5b26c0983af1b60aacd266ecf2b2642a78f2b
LafyOjo <lafybugatti@googlemail.com>
2023-03-31 12:31:39 +0100

    Completed the UI design practice

--- ITC-Assignment-2/ITC-Assignment-2/ViewController.swift
+++ ITC-Assignment-2/ITC-Assignment-2/ViewController.swift
@@ -9,9 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var foodQuestionLabel: UILabel!
+    
+    @IBOutlet weak var foodQuestionLabelAnswerBox: UITextField!
+    
+    @IBOutlet weak var foodSubmitBox: UIButton!
+    
+    @IBOutlet weak var catImage: UIImageView!
+    
+    @IBOutlet weak var identityQuestionLabel: UILabel!
+    
+    @IBOutlet weak var identityQuestionAnswer: UITextField!
+    
+    @IBOutlet weak var identityQuestionSubmitBox: UIButton!
+    
+    @IBOutlet weak var whatIsYourAgeLabel: UILabel!
+    
+    @IBOutlet weak var ageSlider: UISlider!
+    
+    @IBOutlet weak var doYouLikeFantaQuestion: UILabel!
+    
+    @IBOutlet weak var doYoulikeFantaYesButton: UIButton!
+    
+    @IBOutlet weak var DoYouLikeFantaNoButton: UIButton!
+    
+    @IBOutlet weak var LogLabel: UILabel!
+    
+    @IBOutlet weak var DebugLabel: UILabel!
+    
+    @IBOutlet weak var TrackerLabel: UILabel!
+    
+    @IBOutlet var GPSLabel: UIView!
+    
+    @IBOutlet weak var LogSwitchButton: UISwitch!
+    
+    @IBOutlet weak var DebugButtonSwitch: UISwitch!
+    
+    @IBOutlet weak var TrackerLabelSwitch: UISwitch!
+    
+    @IBOutlet weak var GPSLabelSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        foodQuestionLabel.text = "What is your favourite drink?"
+        
+        identityQuestionLabel.text = "What is the image located above."
+        
     }
 
 
